7753e74781bde1e789f3343736c9fdb8947a14ef
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-08-27 13:24:38 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -=1
+    print("Happy New Year!")    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [num ** 2 for num in int_list]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 1
+    while count <= 100:
+        if count % 3 == 0 and count % 5 == 0:
+            print("FizzBuzz")
+        elif count % 3 == 0:
+            print("Fizz")
+        elif count % 5 == 0:
+            print("Buzz")
+        else:
+            print(count)        
+        count +=1
+    
